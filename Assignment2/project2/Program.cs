54d84668fc34ccb371735e10e3449c0667836ae6
brokenmoon <brokenmoon233@gmail.com>
2023-03-10 15:51:04 +0800

    Assignment2 finished.

--- Assignment2/project2/Program.cs
+++ Assignment2/project2/Program.cs
@@ -2,8 +2,29 @@
 using project1;
 namespace project2
 {
-    internal class Program
+    public class Factory
     {
+        public static Rectangle MakeShape(string shapename, int width = 0, int hight = 0, int a = 0, int b = 0, int c = 0)
+        {
+            if(shapename == "Square")
+            {
+                return new Square(width);
+            }
+            else if(shapename == "Oblong")
+            {
+                return new Oblong(width, hight);
+            }
+            else if(shapename == "Triangle")
+            {
+                return new Triangle(a, b, c);
+            }
+            
+            return null;
+        }
+    }
+
+    public class Program
+    { 
         public static void Main(string[] args)
         {
             Random op = new Random();
@@ -17,7 +38,7 @@ namespace project2
                 switch (op.Next(1,4))
                 {
                     case 1:
-                        Square square = new Square(a.Next(0, 100));
+                        Rectangle square = Factory.MakeShape("Square", a.Next(0, 100));
                         Console.WriteLine("The " + square.ShapeName + " is " + square.IsLegal()
                             + " it's area is " + square.GetArea());
                         if (square.IsLegal())
@@ -27,7 +48,7 @@ namespace project2
                         }
                         break;
                     case 2:
-                        Oblong oblong = new Oblong(a.Next(0, 100), b.Next(0, 100));
+                        Rectangle oblong = Factory.MakeShape("Oblong", a.Next(0, 100), b.Next(0, 100));
                         Console.WriteLine("The " + oblong.ShapeName + " is " + oblong.IsLegal()
                             + " it's area is " + oblong.GetArea());
                         if (oblong.IsLegal())
@@ -37,7 +58,7 @@ namespace project2
                         }
                         break;
                     case 3:
-                        Triangle triangle = new Triangle(a.Next(0,100), b.Next(0,100), c.Next(0,100));
+                        Rectangle triangle = Factory.MakeShape("Triangle", 0, 0, a.Next(0, 100), b.Next(0, 100), c.Next(0, 100));
                         Console.WriteLine("The " + triangle.ShapeName + " is " + triangle.IsLegal()
                             + " it's area is " + triangle.GetArea());
                         if (triangle.IsLegal())
