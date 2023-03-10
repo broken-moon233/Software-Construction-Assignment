0b29a56b8038bbbf8b095963b01c0823b8018bb6
brokenmoon <brokenmoon233@gmail.com>
2023-03-10 09:18:22 +0800

    Assignment2 finished.

--- Assignment2/project2/Program.cs
+++ Assignment2/project2/Program.cs
@@ -10,6 +10,8 @@ namespace project2
             Random a = new Random();
             Random b = new Random();
             Random c = new Random();
+            double sumArea = 0;
+            int sumLegal = 0;
             for(int i= 0; i < 10; i++)
             {
                 switch (op.Next(1,4))
@@ -18,19 +20,35 @@ namespace project2
                         Square square = new Square(a.Next(0, 100));
                         Console.WriteLine("The " + square.ShapeName + " is " + square.IsLegal()
                             + " it's area is " + square.GetArea());
+                        if (square.IsLegal())
+                        {
+                            sumLegal += 1;
+                            sumArea += square.GetArea();
+                        }
                         break;
                     case 2:
                         Oblong oblong = new Oblong(a.Next(0, 100), b.Next(0, 100));
                         Console.WriteLine("The " + oblong.ShapeName + " is " + oblong.IsLegal()
                             + " it's area is " + oblong.GetArea());
+                        if (oblong.IsLegal())
+                        {
+                            sumLegal += 1;
+                            sumArea += oblong.GetArea();
+                        }
                         break;
                     case 3:
                         Triangle triangle = new Triangle(a.Next(0,100), b.Next(0,100), c.Next(0,100));
                         Console.WriteLine("The " + triangle.ShapeName + " is " + triangle.IsLegal()
                             + " it's area is " + triangle.GetArea());
+                        if (triangle.IsLegal())
+                        {
+                            sumLegal += 1;
+                            sumArea += triangle.GetArea();
+                        }
                         break;
                 }
             }
+            Console.WriteLine("There are " + sumLegal +" legal shapes. The sum of the areas is " + sumArea);
         }
     }
 }
